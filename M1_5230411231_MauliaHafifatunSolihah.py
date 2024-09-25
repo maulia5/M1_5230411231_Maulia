bc2788de49b8d29bb9c333e411ca6499879d226d
Maulia Hafifatun Solihah <155612320+maulia5@users.noreply.github.com>
2024-09-25 11:27:41 +0700

    Update M1_5230411231_MauliaHafifatunSolihah.py

--- M1_5230411231_MauliaHafifatunSolihah.py
+++ M1_5230411231_MauliaHafifatunSolihah.py
@@ -1,4 +1,4 @@
-# LUAS SEGITIGA
+# LUAS SEGITIGA (2D)
 
 print("-"*10 + " MENGHITUNG LUAS SEGITIGA " + "-"*10)
 
@@ -15,3 +15,20 @@ hitungluas = menghitung_luas_segitiga(alas, tinggi)
 print(f"Luas Segitiga : {hitungluas}")
 
 print("-"*16 + " TERIMA KASIH " + "-"*16)
+
+# LUAS KUBUS (3D)
+
+print("-"*10 + " MENGHITUNG LUAS KUBUS " + "-"*10)
+
+def mengitung_luas_kubus(sisi):
+    luas = 6 * (sisi ** 2)
+    return luas
+
+# input dan hitung
+sisi = float(input("Masukkan Panjang Sisi Kubus : "))
+
+hitungluas_kubus = mengitung_luas_kubus(sisi)
+
+print(f"Luas Permukaan Kubus : {hitungluas_kubus}")
+
+print("-"*15 + " TERIMA KASIH " + "-"*15)
